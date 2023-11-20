3c97af047537d5511ea930d6967d72a70953b6fb
Oceaanic <Oceanmarnoch@gmail.com>
2023-11-19 21:59:55 -0500

    fix __len__ method

--- python/SketchesDataset.py
+++ python/SketchesDataset.py
@@ -3,13 +3,13 @@ import numpy as np
 class SketchesDataset():
     def __init__(self, data_path, transform=None):
         # data_path is path to one of the npz files
-        # transform is not currently used
+        # transform is not currently functional
         self.data_path = data_path
         self.data_set = np.load(data_path, encoding='latin1', allow_pickle=True)
         self.transform = transform
 
     def __len__(self):
-        return len(self.sketchs_file_path)
+        return len(self.data_set['train'])
 
     def __getitem__(self, idx):
         sketch = self.data_set['train'][idx]
