6215d608b52c4e3febd93cf004867d96755a123e
Stuart Fong <72262579+stuartfong1@users.noreply.github.com>
2024-01-14 17:26:19 -0500

    Update sample_from_distribution.py

--- python/sample_from_distribution.py
+++ python/sample_from_distribution.py
@@ -2,9 +2,9 @@
 # sigma is the standard deviation of the latent space distribution
 def sample_latent_vector(mu, sigma_hat):
     # Convert sigma_hat to sigma using an exponential operation
-    sigma = np.exp(sigma_hat / 2.0)
+    sigma = torch.exp(sigma_hat / 2.0)
     # Sample epsilon from a standard normal distribution
-    epsilon = np.random.randn(mu.shape[0])
+    epsilon = torch.randn(sigma.shape)
     # Calculate the latent vector z
-    z = mu + np.multiply(sigma, epsilon)
+    z = mu + sigma * epsilon
     return z
