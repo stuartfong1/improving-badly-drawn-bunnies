cc26cbbb4de16ad3bb087c041a0b72ceeb588795
Stuart Fong <stuartfong1@gmail.com>
2024-02-26 21:19:59 -0500

    Make demo work with model

---
+++ python/display_data.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def display(my_array):
+    my_array = np.array(my_array)
+    #Xplot and Yplot are array of points that will be plotted
+    Xplot = []
+    Yplot = []
+    #Keeps track of the current point that is being drawn
+    xpos = 0
+    ypos = 0
+    #While loop to go through data and plot points
+    i=0
+    while i < len(my_array):    
+        xpos += my_array[i,0]
+        ypos += my_array[i,1]
+        Xplot.append(xpos)
+        Yplot.append(-ypos)
+        if my_array[i,2] == 1:
+            plt.plot(Xplot, Yplot,color='black')
+            Xplot = []
+            Yplot = []
+        i+=1
+    plt.show()
